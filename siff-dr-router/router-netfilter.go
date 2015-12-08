7292cff921beed551212de95c5eef7bf05ed1629
Brett Ammeson <ammesonb@gmail.com>
2015-12-07 19:01:01 -0500

    Adds serialize function and supposedly reinjects modified packet

--- siff-dr-router/router-netfilter.go
+++ siff-dr-router/router-netfilter.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"log"
+    "bytes"
 
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/layers"
 	"github.com/ThomasJClark/cs4516project/pkg/go-netfilter-queue"
 )
+
+func Serialize(ipLayer *layers.IPv4) ([]byte, error) {
+    /*Write the IPv4 header into a gopacket buffer*/
+    buf := gopacket.NewSerializeBuffer()
+    err := ipLayer.SerializeTo(buf, gopacket.SerializeOptions{FixLengths: false, ComputeChecksums: true})
+    if err != nil {
+        return nil, err
+    }
+
+    /*Write the gopacket buffer and the payload into a byte buffer, concatenating
+    the entire packet together.*/
+    var buf2 bytes.Buffer
+    buf2.Write(buf.Bytes())
+    buf2.Write(ipLayer.Payload)
+
+    return buf2.Bytes(), nil
+}
 
 func processPackets() {
 	nfq, err := netfilter.NewNFQueue(0, 100000, 0xffff)
@@ -25,7 +45,23 @@ func processPackets() {
                 }
             }
             shiftCapability(&packet)
-		    packet.SetVerdict(netfilter.NF_ACCEPT)
+
+            var ipLayer *layers.IPv4
+            /* Get the IPv4 layer, and if it doesn't exist, keep doing shit
+            I can't be arsed for proper response outside the bounds of this project */
+            if layer := packet.Packet.Layer(layers.LayerTypeIPv4); layer != nil {
+                ipLayer = layer.(*layers.IPv4)
+            } else  {
+                // maybe do something?
+            }
+
+            serializedPacket, err := Serialize(ipLayer)
+            if err != nil {
+                log.Println("Failed to serialize packet, dropping")
+                packet.SetVerdict(netfilter.NF_DROP)
+            } else {
+		        packet.SetResult(netfilter.NF_ACCEPT, serializedPacket)
+            }
         } else {
 		    packet.SetVerdict(netfilter.NF_ACCEPT)
         }
